51d75d23996362453cceb1f55e76b805e93819c5
Jani Basha Shaik <jani.ham@gmail.com>
2022-02-13 21:44:47 -0500

    Extra visibility changes added
    - Added view visibility changes
    - Added toast for the sign in button

--- app/src/main/java/com/example/signupapplication/ConfirmationFragment.kt
+++ app/src/main/java/com/example/signupapplication/ConfirmationFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.example.signupapplication.databinding.FragmentConfirmationBinding
 
@@ -27,12 +28,17 @@ class ConfirmationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        sharedViewModel.getRegisteredUser()?.let {
+        sharedViewModel. getRegisteredUser()?.let {
             binding.nameId.text = it.firstName
             binding.emailId.text = it.email
             binding.websiteId.text = it.webSite
 
             binding.nameTitle.text = resources.getString(R.string.name_header, it.firstName)
+        }
+        sharedViewModel.changeNameVisibility()
+
+        binding.signBtn.setOnClickListener {
+            Toast.makeText(requireContext(),"Sign in Clicked!", Toast.LENGTH_SHORT).show()
         }
 
     }
--- app/src/main/java/com/example/signupapplication/SignUpSharedViewModel.kt
+++ app/src/main/java/com/example/signupapplication/SignUpSharedViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.signupapplication
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.example.signupapplication.model.User
 
@@ -11,6 +13,22 @@ class SignUpSharedViewModel: ViewModel() {
     fun setUserDetails(user: User) {
         _registeredUser.value = user
     }
+
     fun getRegisteredUser() = _registeredUser.value
+
+    private val _isVisible: MutableLiveData<Boolean> = MutableLiveData<Boolean>(true)
+    val isVisible
+        get() = Transformations.map(_isVisible) {
+            when (it) {
+                true -> View.VISIBLE
+                else -> View.INVISIBLE
+            }
+        }
+
+    fun changeNameVisibility() {
+        getRegisteredUser()?.let {
+            _isVisible.value = it.firstName.isNotEmpty()
+        }
+    }
 
 }
